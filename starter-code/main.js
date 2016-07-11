9d4d90721bd21f866c015f5af120bf63156c22e8
Leo Burke <Leo Burke>
2016-07-11 02:36:38 -0700

    finishing nightly work

--- starter-code/main.js
+++ starter-code/main.js
@@ -9,8 +9,28 @@ var selectedCardOne = cardOne;
 var selectedCardTwo = cardThree;
 
 if(selectedCardOne === selectedCardTwo){
-	alert("match");
+	//alert("match");
 }
 else{
-	alert("try again");
+	//alert("try again");
+}
+
+var board = document.getElementById('game-board');
+
+
+makeCards(3);
+
+var fuck = function(){
+	console.log("Fuck");
+}
+
+
+function makeCards(numberOfCards){
+	
+	for(var i = 0; i < numberOfCards; i += 1){
+		console.log("made a card");
+		var tempCard = document.createElement('div');
+		tempCard.className = 'card';
+		document.getElementById("gameboard").appendChild(tempCard);
+	} 
 }
